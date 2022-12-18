188b534fef81d71f446e5e87e45e101039be3777
Akshay Manthekar <58980814+Akshay4452@users.noreply.github.com>
2022-12-18 12:38:38 +0530

    Screen Wrap and Player Rotation Smoothened
    Screen has been wrapped so that spaceship will not disappear from game. Also, spaceship rotation has been dealt with Quaternio.Lerp so that it would linearly interpolate between initial and final rotation position.

--- Asteroid Avoider/Assets/Scripts/PlayerMovement.cs
+++ Asteroid Avoider/Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,8 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float forceMagnitude;
     [SerializeField] float maxSpeed;
-    private Camera mainCamera;
+    [SerializeField] float rotationSpeed;
+    Camera mainCamera;
     new Rigidbody rigidbody;
     private Vector3 movementDirection;
 
@@ -20,25 +21,11 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        if(Touchscreen.current.primaryTouch.press.isPressed)
-        {
-            // Getting touch position
-            Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
-            // Debug.Log($"Touch position (screen units): {touchPosition}");
+        ProcessInput();
 
-            // we will use camera to convert screen space to world space
-            Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
-            // Debug.Log($"Touch position (World units): {worldPosition}");
+        KeepPlayerOnScreen();
 
-            movementDirection = transform.position - worldPosition;
-            movementDirection.z = 0; // so that spaceship wont move inside or outside of the screen
-
-            movementDirection.Normalize(); // if its magnitude increases, speed can also change very quickly
-        }
-        else 
-        {
-            movementDirection = Vector3.zero;
-        }   
+        RotateToFaceVelocity();
     }
     
     void FixedUpdate() 
@@ -49,5 +36,60 @@ public class PlayerMovement : MonoBehaviour
 
         // Restrict velocity to the max speed specified in the inspector
         rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, maxSpeed);
+    }
+
+    private void ProcessInput()
+    {
+        if(Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            // Getting touch position
+            Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+
+            // we will use camera to convert screen space to world space
+            Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
+
+            movementDirection = transform.position - worldPosition;
+            movementDirection.z = 0; // so that spaceship wont move inside or outside of the screen
+
+            movementDirection.Normalize(); // if its magnitude increases, speed can also change very quickly
+        }
+        else 
+        {
+            movementDirection = Vector3.zero;
+        } 
+    }
+
+    private void KeepPlayerOnScreen()
+    {
+        Vector3 newPosition = transform.position;
+
+        Vector3 viewportPosition = mainCamera.WorldToViewportPoint(transform.position);
+
+        if(viewportPosition.x > 1)
+        {
+            newPosition.x = -newPosition.x + 0.1f;
+        }
+        else if(viewportPosition.x < 0)
+        {
+            newPosition.x = -newPosition.x - 0.1f;
+        }
+        if(viewportPosition.y > 1)
+        {
+            newPosition.y = -newPosition.y + 0.1f;
+        }
+        else if(viewportPosition.y < 0)
+        {
+            newPosition.y = -newPosition.y - 0.1f;
+        }
+
+        transform.position = newPosition;
+    }
+    private void RotateToFaceVelocity()
+    {
+        if (rigidbody.velocity == Vector3.zero) { return;}
+        
+        Quaternion targetRotation = Quaternion.LookRotation(rigidbody.velocity, Vector3.back);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed*Time.deltaTime);
     }
 }
